03865d74e7169d2b90d1c2103599eb0a325b3d06
Andrei <rabota.laptev@mail.ru>
2018-10-06 11:34:39 +0700

    order

--- src/TsvNews/Repository/TsvNewsRepository.php
+++ src/TsvNews/Repository/TsvNewsRepository.php
@@ -9,9 +9,9 @@ class TsvNewsRepository extends EntityRepository
 	public function getFilteredNews($archive_date = '', $em)
 	{
 		if($archive_date=='')
-			$query = $em->createQuery("SELECT n FROM TsvNews\Entity\News n WHERE n.disabled_news = 0 and n.start_date <= '".date("Y-m-d H:i:s")."' and n.end_date >= '".date("Y-m-d H:i:s")."'");
+			$query = $em->createQuery("SELECT n FROM TsvNews\Entity\News n WHERE n.disabled_news = 0 and n.start_date <= '".date("Y-m-d H:i:s")."' and n.end_date >= '".date("Y-m-d H:i:s")."'  order by n.news_date DESC");
 		else 
-			$query = $em->createQuery("SELECT n FROM TsvNews\Entity\News n WHERE n.disabled_news = 0 and n.news_date >= '".date("Y-m-d H:i:s",strtotime($archive_date.'-01'))."' and n.news_date <= '".date("Y-m-01 H:i:s",strtotime($archive_date.'-01')+60*60*24*32)."'");
+			$query = $em->createQuery("SELECT n FROM TsvNews\Entity\News n WHERE n.disabled_news = 0 and n.news_date >= '".date("Y-m-d H:i:s",strtotime($archive_date.'-01'))."' and n.news_date <= '".date("Y-m-01 H:i:s",strtotime($archive_date.'-01')+60*60*24*32)."' order by n.news_date DESC");
 		
 		return $query;
 	}
